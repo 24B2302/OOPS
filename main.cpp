a6170bb289a7f34b472519015015c5bba10a3195
24B2302 <moniajain23@gmail.com>
2025-01-06 19:16:57 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Car{
+    public :
+//attributes 
+
+string make;
+string model;
+float price;
+
+//methods
+
+int apply_discount(float price)
+}
+int main(
+    
+    Car car1;
+    Car car2;
+    Car car3;
+    Car car4;
+    
+    
+    
+   car1.make= Toyota;
+   car1.model= Nissan;
+   car1.price = 1000000;
+   
+   car2.make = Hyundai ;
+   car2.make = Santro;
+   car2.price = 800000;
+   
+   cout<<car1.name;
+   //likewise print all the attributes of every object in the Car class //
+    return 0;
+    )
